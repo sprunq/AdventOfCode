eae118312c72758806eecc8242af1d75625c20de
sprunq <tobias.laurin@web.de>
2022-12-03 15:56:47 +0100

    optimized

--- AoC2022/src/aoc3/mod.rs
+++ AoC2022/src/aoc3/mod.rs
@@ -32,26 +32,25 @@ pub fn p1() {
 }
 
 pub fn p2() {
-    let input = fs::read_to_string("src\\aoc3\\input.txt").expect("Cannot find file");
-    let mut matches = input
-        .split('\n')
-        .map(|f| f.chars().collect::<Vec<_>>())
+    let matches = include_bytes!("input.txt")
+        .split(|b| *b == b'\n')
         .collect::<Vec<_>>();
 
-    let mut sum = 0;
-    for str_tup in matches.chunks(3) {
-        let x = str_tup[0]
-            .iter()
-            .find(|f| str_tup[0].contains(f) && str_tup[0].contains(f))
-            .unwrap();
+    let sum: u16 = matches
+        .chunks(3)
+        .map(|chunk| {
+            let x = chunk[0]
+                .iter()
+                .find(|f| chunk[1].contains(f) && chunk[2].contains(f))
+                .unwrap();
 
-        let ch = *x as char;
-        let mut char_prio = ch.to_digit(36).unwrap() - 9;
-        if ch.is_uppercase() {
-            char_prio += 26;
-        }
-        sum += char_prio;
-    }
+            if *x >= b'a' {
+                (x - b'a') as u16 + 1
+            } else {
+                (x - b'A') as u16 + 27
+            }
+        })
+        .sum();
 
     println!("{}", sum);
 }
