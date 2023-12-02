1f930b61d5ed6187d41107efa3391234dce1af56
sprunq <52038911+sprunq@users.noreply.github.com>
2023-12-02 11:15:59 +0100

    day2

--- AoC2023/src/days/aoc02/mod.rs
+++ AoC2023/src/days/aoc02/mod.rs
@@ -1,71 +1,106 @@
 crate::AocDay!(2);
 
-use itertools::Itertools;
+const INPUT: &str = include_str!("input.txt");
 
 pub fn part_1() -> String {
-    let res = include_bytes!("input.txt")
-        .split(|&c| c == b'\n')
-        .collect_vec();
+    let parsed = parse_input(INPUT);
+    const LIMIT_RED: usize = 12;
+    const LIMIT_GREEN: usize = 13;
+    const LIMIT_BLUE: usize = 14;
 
-    let sum: u32 = res
-        .into_iter()
-        .map(|line| {
-            let first = line.iter().find(|c| c.is_ascii_digit()).unwrap();
-            let last = line.iter().rfind(|c| c.is_ascii_digit()).unwrap();
-            (first - b'0') as u32 * 10 + (last - b'0') as u32
+    let possibe_games: usize = parsed
+        .iter()
+        .enumerate()
+        .filter_map(|(id, game)| {
+            let possible_games = game
+                .iter()
+                .filter(|draw| {
+                    draw.red <= LIMIT_RED && draw.green <= LIMIT_GREEN && draw.blue <= LIMIT_BLUE
+                })
+                .count();
+
+            if possible_games == game.len() {
+                Some(id + 1)
+            } else {
+                None
+            }
         })
         .sum();
 
-    format!("{}", sum)
+    format!("{}", possibe_games)
 }
 
 pub fn part_2() -> String {
-    let lines = include_str!("input.txt").lines();
+    let parsed = parse_input(INPUT);
 
-    let mut sum = 0;
-    for mut line in lines {
-        let first = 'out: loop {
-            for (word, num) in NUMBER_MAP.iter() {
-                if line.starts_with(word) {
-                    break 'out *num as u32;
-                }
-            }
-            line = &line[1..];
-        };
+    let possibe_games: usize = parsed
+        .iter()
+        .map(|game| {
+            let max_red = game.iter().map(|draw| draw.red).max().unwrap();
+            let max_green = game.iter().map(|draw| draw.green).max().unwrap();
+            let max_blue = game.iter().map(|draw| draw.blue).max().unwrap();
 
-        let last = 'out: loop {
-            for (word, num) in NUMBER_MAP.iter() {
-                if line.ends_with(word) {
-                    break 'out *num as u32;
-                }
-            }
-            line = &line[..line.len() - 1];
-        };
-        sum += first * 10 + last;
-    }
+            max_red * max_green * max_blue
+        })
+        .sum();
 
-    format!("{}", sum)
+    format!("{}", possibe_games)
 }
 
-const NUMBER_MAP: [(&str, u8); 20] = [
-    ("0", 0),
-    ("1", 1),
-    ("2", 2),
-    ("3", 3),
-    ("4", 4),
-    ("5", 5),
-    ("6", 6),
-    ("7", 7),
-    ("8", 8),
-    ("9", 9),
-    ("zero", 0),
-    ("one", 1),
-    ("two", 2),
-    ("three", 3),
-    ("four", 4),
-    ("five", 5),
-    ("six", 6),
-    ("seven", 7),
-    ("eight", 8),
-    ("nine", 9),
-];
+fn parse_input(input: &str) -> Vec<Vec<GameDraw>> {
+    let mut games = Vec::new();
+    for line in input.lines() {
+        let (_, game) = line.split_once(":").unwrap();
+        let drawings = game.split(";");
+        let mut game_draws = Vec::new();
+        for drawing in drawings {
+            game_draws.push(parse_drawing(drawing));
+        }
+        games.push(game_draws);
+    }
+    games
+}
+
+fn parse_drawing(input: &str) -> GameDraw {
+    let split = input.split(",");
+    let mut draw = GameDraw::empty();
+    for s in split {
+        let (digit, color) = s.trim().split_once(" ").unwrap();
+        let number = digit.trim().parse::<usize>().unwrap();
+
+        match color.trim() {
+            "red" => {
+                draw.red = number;
+            }
+            "blue" => {
+                draw.blue = number;
+            }
+            "green" => {
+                draw.green = number;
+            }
+            _ => panic!("Invalid color"),
+        }
+    }
+    draw
+}
+
+#[derive(Debug, Clone)]
+struct GameDraw {
+    red: usize,
+    green: usize,
+    blue: usize,
+}
+
+impl GameDraw {
+    fn new(red: usize, green: usize, blue: usize) -> Self {
+        Self { red, green, blue }
+    }
+
+    fn empty() -> Self {
+        Self {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+}
