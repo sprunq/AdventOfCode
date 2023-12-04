c30b0513a3e699296938200307845c517f5ada90
sprunq <52038911+sprunq@users.noreply.github.com>
2023-12-04 11:52:19 +0100

    p2

--- AoC2023/src/days/aoc04/mod.rs
+++ AoC2023/src/days/aoc04/mod.rs
@@ -9,16 +9,10 @@ pub fn part_1() -> String {
         let winning_num = parse_numbers::<10>(&line[10..39]);
         let guessed_num = parse_numbers::<25>(&line[42..116]);
 
-        let mut correct = 0;
-        for i in 0..winning_num.len() {
-            for j in 0..guessed_num.len() {
-                if winning_num[i] == guessed_num[j] {
-                    correct += 1;
-                }
-            }
-        }
+        let correct = get_correct_score(winning_num, guessed_num);
+
         if correct > 0 {
-            let line_score = 2_i32.pow(correct - 1);
+            let line_score = 2_i32.pow(correct as u32 - 1);
             score += line_score;
         }
     }
@@ -29,27 +23,15 @@ pub fn part_1() -> String {
 #[inline(always)]
 pub fn part_2() -> String {
     let lines = INPUT.split(|b| *b == b'\n').collect::<Vec<_>>();
-    let mut copies = vec![0; lines.len()];
+    let mut copies = vec![1; lines.len()];
     for (idx, line) in lines.iter().enumerate() {
         let winning_num = parse_numbers::<10>(&line[10..39]);
         let guessed_num = parse_numbers::<25>(&line[42..116]);
 
-        let mut correct = 0;
-        for i in 0..winning_num.len() {
-            for j in 0..guessed_num.len() {
-                if winning_num[i] == guessed_num[j] {
-                    correct += 1;
-                }
-            }
-        }
+        let correct = get_correct_score(winning_num, guessed_num);
 
-        if correct > 0 {
-            let current_copy_count = copies[idx];
-
-            // add copy count to next cards
-            for x in idx + 1..idx + correct + 1 {
-                copies[x] += 1 * current_copy_count;
-            }
+        for underlying_card_idx in idx + 1..idx + 1 + correct {
+            copies[underlying_card_idx] += copies[idx];
         }
     }
 
@@ -57,6 +39,23 @@ pub fn part_2() -> String {
     format!("{}", sum)
 }
 
+#[inline(always)]
+fn get_correct_score<const N1: usize, const N2: usize>(
+    winning_num: [u8; N1],
+    guessed_num: [u8; N2],
+) -> usize {
+    let mut correct = 0;
+    for i in 0..N1 {
+        for j in 0..N2 {
+            if winning_num[i] == guessed_num[j] {
+                correct += 1;
+            }
+        }
+    }
+    correct
+}
+
+#[inline(always)]
 fn parse_numbers<const N: usize>(input: &[u8]) -> [u8; N] {
     let mut numbers = [0; N];
     for i in 0..N {
